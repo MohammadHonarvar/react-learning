584d7a8c15c92c1a270eeda7acf88aca2762efe8
Mohammad Honarvar <honarvar.info@gmail.com>
2021-03-12 23:03:51 +0330

    review(traditional): use list templates & cleanu up codes

--- src/traditional/App.tsx
+++ src/traditional/App.tsx
@@ -1,98 +1,137 @@
-import { ChangeEvent, Component } from 'react';
+import { ChangeEvent, MouseEvent, Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-// import { person as Person } from './Person';
+import { person as Person } from './Person';
 
-import { userInput as UserInput } from './UserInput';
-import { userOutput as UserOutput } from './UserOutput';
+// import { userInput as UserInput } from './UserInput';
+// import { userOutput as UserOutput } from './UserOutput';
+
+class App extends Component {
+  state = {
+    personList: [
+      {
+        id: 'asd1',
+        name: 'MHF',
+        age: 25,
+      },
+      {
+        id: 'asd11',
+        name: 'MHF2',
+        age: 28,
+      },
+      {
+        id: 'asd111',
+        name: 'MHF3',
+        age: 30,
+      },
+    ],
+    isExistInList: false,
+    showPersonList: false,
+  };
+
+  changePerson = (event: ChangeEvent<HTMLInputElement>, personId: string) => {
+    console.log('Name: %s', personId);
+    const targetPersonIndex = this.state.personList.findIndex(
+      (person) => person.id === personId
+    );
+    if (targetPersonIndex === -1) return;
+
+    const personList = [...this.state.personList];
+
+    const targetPerson = {
+      ...this.state.personList[targetPersonIndex],
+    };
+    targetPerson.name = event.target.value;
+
+    personList[targetPersonIndex] = targetPerson;
+
+    this.setState({
+      personList,
+    });
+  };
+
+  togglePersonListDisplay = () => {
+    this.setState({
+      showPersonList: !this.state.showPersonList,
+    });
+  };
+
+  deletePerson = (event: MouseEvent<HTMLElement>, index: number) => {
+    const personList = [...this.state.personList];
+    personList.splice(index, 1);
+    this.setState({
+      personList,
+    });
+  };
+
+  render() {
+    const buttonStyle = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+    };
+
+    return (
+      <div className="App">
+        <h1>This my REACT App</h1>
+        <button style={buttonStyle} onClick={this.togglePersonListDisplay}>
+          Toggle Person List
+        </button>
+        {this.getPersonListTemplate()}
+      </div>
+    );
+  }
+
+  getPersonListTemplate() {
+    let personListTemplate = null;
+    if (this.state.showPersonList) {
+      personListTemplate = (
+        <div>
+          {this.state.personList.map((person, index) => {
+            return (
+              <Person
+                key={person.id}
+                name={person.name}
+                age={person.age}
+                click={(event) => this.deletePerson(event, index)}
+                nameChangedHandler={(event) => this.changePerson(event, person.id)}
+              />
+            );
+          })}
+        </div>
+      );
+    }
+
+    return personListTemplate;
+  }
+}
 
 // class App extends Component {
 //   state = {
-//     personList: [
-//       {
-//         name: 'MHF',
-//         age: 25,
-//       },
-//       {
-//         name: 'MHF2',
-//         age: 28,
-//       },
-//       {
-//         name: 'MHF3',
-//         age: 30,
-//       },
-//     ],
-//     iExistInList: false,
+//     userName: 'MHFFF',
 //   };
 
-//   changePerson = () => {
+//   userNameChangedHandler = (event: ChangeEvent<HTMLInputElement>) => {
 //     this.setState({
-//       personList: [
-//         {
-//           name: 'Honarvar',
-//           age: 25,
-//         },
-//         {
-//           name: 'MHF2',
-//           age: 28,
-//         },
-//         {
-//           name: 'Farahbakhsh',
-//           age: 30,
-//         },
-//       ],
+//       userName: event.target.value,
 //     });
 //   };
 
 //   render() {
 //     return (
 //       <div className="App">
-//         <h1>This my REACT App</h1>
-//         <button onClick={this.changePerson}>New Person</button>
-//         <Person
-//           name={this.state.personList[0].name}
-//           age={this.state.personList[0].age}
-//           click={this.changePerson}
+//         <UserInput
+//           userNameChangedHandler={this.userNameChangedHandler}
+//           defaultUserName={this.state.userName ?? 'mhf'}
 //         />
-//         <Person
-//           name={this.state.personList[1].name}
-//           age={this.state.personList[1].age}
-//         />
-//         <Person
-//           name={this.state.personList[2].name}
-//           age={this.state.personList[2].age}
-//         >
-//           This is my favorites: PS5
-//         </Person>
+//         <UserOutput userName={this.state.userName} />
+//         <UserOutput />
+//         <UserOutput userName="MHF3" />
 //       </div>
 //     );
 //   }
 // }
-
-class App extends Component {
-  state = {
-    userName: 'MHFFF',
-  };
-
-  userNameChangedHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    this.setState({
-      userName: event.target.value,
-    });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <UserInput
-          userNameChangedHandler={this.userNameChangedHandler}
-          defaultUserName={this.state.userName ?? 'mhf'}
-        />
-        <UserOutput userName={this.state.userName} />
-        <UserOutput />
-        <UserOutput userName="MHF3" />
-      </div>
-    );
-  }
-}
 
 export default App;
--- src/traditional/Person/Person.tsx
+++ src/traditional/Person/Person.tsx
@@ -1,15 +1,19 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { PropsWithChildren } from 'react';
+import React, { PropsWithChildren } from 'react';
+
+import './Person.css';
 
 export const person = (
   props: PropsWithChildren<{
     name: string;
     age: number;
     click?: React.MouseEventHandler<HTMLParagraphElement>;
+    nameChangedHandler?: React.ChangeEventHandler<HTMLInputElement>;
   }>
 ) => {
   return (
-    <div>
+    <div className="Person">
+      <input type="text" value={props.name} onChange={props.nameChangedHandler} />
       <p onClick={props.click}>
         I'm {props.name} and i'm {props.age} years old
       </p>
